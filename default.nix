99ce08783499f6ffc61449401bcf252ca845c265
haslersn <sebastian.hasler@gmx.net>
2019-06-25 18:17:55 +0200

    default.nix: Modernize and cargo update

--- default.nix
+++ default.nix
@@ -1,38 +1,42 @@
-{ pkgsPath ? <nixpkgs>, crossSystem ? null }:
-
 let
-    mozOverlay = import (
-        builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
-    );
-    pkgs = import pkgsPath {
-        overlays = [ mozOverlay ];
-        inherit crossSystem;
-    };
-    targets = [ pkgs.stdenv.targetPlatform.config ];
+  defaultPkgs = import <nixpkgs> {};
 in
 
-with pkgs;
+{
+  fetchFromGitHub ? defaultPkgs.fetchFromGitHub,
+  makeRustPlatform ? defaultPkgs.makeRustPlatform,
+  openssl ? defaultPkgs.openssl,
+  pkgs ? defaultPkgs
+}:
 
-stdenv.mkDerivation {
-    name = "castle";
+let
+  mozRepo = fetchFromGitHub {
+    owner = "mozilla";
+    repo = "nixpkgs-mozilla";
+    rev = "50bae918794d3c283aeb335b209efd71e75e3954";
+    sha256 = "07b7hgq5awhddcii88y43d38lncqq9c8b2px4p93r5l7z0phv89d";
+  };
+  moz = import "${mozRepo}/package-set.nix" { inherit pkgs; };
+  nightlyChannel = moz.latest.rustChannels.nightly;
+  nightlyRustPlatform = makeRustPlatform {
+    rustc = nightlyChannel.rust;
+    cargo = nightlyChannel.cargo;
+  };
+in
 
-    # build time dependencies targeting the build platform
-    depsBuildBuild = [
-        buildPackages.stdenv.cc
-    ];
-    HOST_CC = "cc";
+nightlyRustPlatform.buildRustPackage rec {
+  name = "castle-${version}";
+  version = "unstable";
 
-    # build time dependencies targeting the host platform
-    nativeBuildInputs = [
-        (buildPackages.buildPackages.latest.rustChannels.nightly.rust.override { inherit targets; })
-        buildPackages.buildPackages.rustfmt
-    ];
-    shellHook = ''
-        export RUSTFLAGS="-C linker=$CC"
-    '';
-    CARGO_BUILD_TARGET = targets;
+  src = ./.;
 
-    # run time dependencies
-    OPENSSL_DIR = openssl_1_1.dev;
-    OPENSSL_LIB_DIR = "${openssl_1_1.out}/lib";
+  cargoSha256 = "15bv2pqwmp0bs7nxvwk23nf5zx77wwiz8l272f5431gfcplx2rrm";
+
+  preConfigure = ''
+    export HOME=$(mktemp -d)
+  '';
+
+  buildInputs = [
+    openssl
+  ];
 }
